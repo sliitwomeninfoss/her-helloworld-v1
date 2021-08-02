898fab771d1f30e9acdf5070c9316b1f20f17398
Sewvandiii <sewvandipw.98@gmail.com>
2021-08-02 08:44:56 +0530

    Minor fix

--- src/components/JudgesSection.tsx
+++ src/components/JudgesSection.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import SewvandiImage from "../assets/images/speakers/sew.jpg";
+import Nadun from "../assets/images/speakers/Nadun.jpg";
+import Nuwini from "../assets/images/speakers/Nuwini.jpg";
+import Nadee from "../assets/images/speakers/Nadee.jpg";
+import Nimesh from "../assets/images/speakers/Nimesh.jpg";
 
 function JudgesSection() {
   return (
@@ -16,227 +19,68 @@ function JudgesSection() {
                   Our Judges
                 </h2>
                 <p className="wow fadeInDown" data-wow-delay="0.2s">
-                  her&lt;Hello World/&gt; delivers a series of judges designed to inspire and equip.
+                  her&lt;Hello World/&gt; delivers a series of judges designed
+                  to inspire and equip.
                 </p>
               </div>
             </div>
           </div>
           <div className="row justify-content-bt">
-            <div className="revealing-soon-text">Judges will be revealing soon... </div>
-            {/* <div className="col-lg-3 col-md-6 col-xs-12"> */}
-            {/* <!-- Team Item Starts --> */}
-            {/* <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
+            <div className="revealing-soon-text">
+            </div>
+            <div className="col-lg-3 col-md-6 col-xs-12">
+              <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
                 <div className="team-img">
-                  <img className="img-fluid" src={SewvandiImage} alt="" />
-                  <div className="team-overlay">
-                    <div className="overlay-social-icon text-center">
-                      <ul className="social-icons">
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-twitter-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-google" aria-hidden="true" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-facebook-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-pinterest" aria-hidden="true" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
+                  <img className="img-fluid" src={Nadun} alt="" />
+                </div>
+                <div className="info-text">
+                <h3>
+                    <h6>Nandun Thilina Bandara</h6>
+                  </h3>
+                  <p>Senior Software Engineer at CareHires</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-xs-12">
+              <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
+                <div className="team-img">
+                  <img className="img-fluid" src={Nuwini} alt="" />
                 </div>
                 <div className="info-text">
                   <h3>
-                    <a href="#">SEWVANDI WICKRAMASINGHE</a>
+                    <h6>Nuwini Chamindi</h6>
                   </h3>
-                  <p>
-                    SLIIT Women In FOSS Community Lead
-                  </p>
+                  <p>WTM Sri Lanka Ambassador at Women Techmakers</p>
                 </div>
-              </div> */}
-            {/* <!-- Team Item Ends --> */}
-            {/* </div> */}
-            {/*  <div className="col-lg-3 col-md-6 col-xs-12"> */}
-            {/* <!-- Team Item Starts --> */}
-            {/* <div className="team-item wow fadeInUp" data-wow-delay="0.4s">
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-xs-12">
+              <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
                 <div className="team-img">
-                  <img
-                    className="img-fluid"
-                    src="assets/img/team/team-02.jpg"
-                    alt=""
-                  />
-                  <div className="team-overlay">
-                    <div className="overlay-social-icon text-center">
-                      <ul className="social-icons">
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-twitter-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-google" aria-hidden="true" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-facebook-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-pinterest" aria-hidden="true" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
+                  <img className="img-fluid" src={Nimesh} alt="" />
                 </div>
                 <div className="info-text">
                   <h3>
-                    <a href="#">Patric Green</a>
+                    <h6>Nimesh Indimagedara</h6>
                   </h3>
-                  <p>Front-end Developer</p>
+                  <p>Founder and CEO of Codebrix (Pvt) Ltd</p>
                 </div>
-              </div> */}
-            {/* <!-- Team Item Ends --> */}
-            {/* </div> */}
-
-            {/*  <div className="col-lg-3 col-md-6 col-xs-12"> */}
-            {/* <!-- Team Item Starts --> */}
-            {/* <div className="team-item wow fadeInUp" data-wow-delay="0.6s">
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-xs-12">
+              <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
                 <div className="team-img">
-                  <img
-                    className="img-fluid"
-                    src="assets/img/team/team-03.jpg"
-                    alt=""
-                  />
-                  <div className="team-overlay">
-                    <div className="overlay-social-icon text-center">
-                      <ul className="social-icons">
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-twitter-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-google" aria-hidden="true" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-facebook-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-pinterest" aria-hidden="true" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
+                  <img className="img-fluid" src={Nadee} alt="" />
                 </div>
                 <div className="info-text">
                   <h3>
-                    <a href="#">Paul Kowalsy</a>
+                    <h6>Nadee Pragnarathne</h6>
                   </h3>
-                  <p>Lead Designer, TNW</p>
+                  <p>Manager, Industry Liaison Center, The Open University of Sri Lanka</p>
                 </div>
-              </div> */}
-            {/* <!-- Team Item Ends --> */}
-            {/* </div> */}
-
-            {/* <div className="col-lg-3 col-md-6 col-xs-12"> */}
-            {/* <!-- Team Item Starts --> */}
-            {/* <div className="team-item wow fadeInUp" data-wow-delay="0.8s">
-                <div className="team-img">
-                  <img
-                    className="img-fluid"
-                    src="assets/img/team/team-04.jpg"
-                    alt=""
-                  />
-                  <div className="team-overlay">
-                    <div className="overlay-social-icon text-center">
-                      <ul className="social-icons">
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-twitter-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-google" aria-hidden="true" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-facebook-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-pinterest" aria-hidden="true" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div className="info-text">
-                  <h3>
-                    <a href="#">Jhon Doe</a>
-                  </h3>
-                  <p>Back-end Developer, ASUS</p>
-                </div>
-              </div> */}
-            {/* <!-- Team Item Ends --> */}
-            {/*  </div> */}
-
+              </div>
+            </div>
           </div>
-          {/* <a
-            href="#"
-            className="btn btn-common mt-30 wow fadeInUp"
-            data-wow-delay="1.9s"
-            style={{ color: "white" }}
-          >
-            Meet all Judges
-          </a> */}
         </div>
       </section>
     </>
--- src/components/SpeakersSection.tsx
+++ src/components/SpeakersSection.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import SewvandiImage from "../assets/images/speakers/sew.jpg";
+import Sewvandi from "../assets/images/speakers/Sewvandi.jpg";
+import Chenuka from "../assets/images/speakers/Chenuka.jpg";
+import Chamodi from "../assets/images/speakers/Chamodi.jpg";
+import Bagya from "../assets/images/speakers/Bhagya.jpeg";
 
 function SpeakersSection() {
   return (
@@ -16,230 +19,69 @@ function SpeakersSection() {
                   Our Speakers
                 </h2>
                 <p className="wow fadeInDown" data-wow-delay="0.2s">
-                  The her&lt;Hello World/&gt; Groom Up Programs connect young women who want to expand their knowledge and gain footing in the tech world with upper-years <br />
-                  who have that experience and knowledge as part of a year-long project development program
+                  The her&lt;Hello World/&gt; Groom Up Programs connect young
+                  women who want to expand their knowledge and gain footing in
+                  the tech world with upper-years <br />
+                  who have that experience and knowledge as part of a year-long
+                  project development program
                 </p>
               </div>
             </div>
           </div>
           <div className="row justify-content-bt">
-            <div className="revealing-soon-text">Speakers will be revealing soon... </div>
-            {/* <div className="col-lg-3 col-md-6 col-xs-12"> */}
-            {/* <!-- Team Item Starts --> */}
-            {/* <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
+          <div className="col-lg-3 col-md-6 col-xs-12">
+              <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
                 <div className="team-img">
-                  <img className="img-fluid" src={SewvandiImage} alt="" />
-                  <div className="team-overlay">
-                    <div className="overlay-social-icon text-center">
-                      <ul className="social-icons">
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-twitter-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-google" aria-hidden="true" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-facebook-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-pinterest" aria-hidden="true" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
+                  <img className="img-fluid" src={Sewvandi} alt="" />
+                </div>
+                <div className="info-text">
+                <h3>
+                    <h6>Sewvandi Wickramasinghe</h6>
+                  </h3>
+                  <p>Community Lead at SLIIT Women In FOSS Community</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-xs-12">
+              <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
+                <div className="team-img">
+                  <img className="img-fluid" src={Chenuka} alt="" />
                 </div>
                 <div className="info-text">
                   <h3>
-                    <a href="#">SEWVANDI WICKRAMASINGHE</a>
+                    <h6>Chenuka Sumanasekara</h6>
                   </h3>
-                  <p>
-                    SLIIT Women In FOSS Community Lead
-                  </p>
+                  <p>Project Coordinator at SLIIT FOSS Community</p>
                 </div>
-              </div> */}
-            {/* <!-- Team Item Ends --> */}
-            {/* </div> */}
-            {/* <div className="col-lg-3 col-md-6 col-xs-12"> */}
-            {/* <!-- Team Item Starts --> */}
-            {/* <div className="team-item wow fadeInUp" data-wow-delay="0.4s">
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-xs-12">
+              <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
                 <div className="team-img">
-                  <img
-                    className="img-fluid"
-                    src="assets/img/team/team-02.jpg"
-                    alt=""
-                  />
-                  <div className="team-overlay">
-                    <div className="overlay-social-icon text-center">
-                      <ul className="social-icons">
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-twitter-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-google" aria-hidden="true" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-facebook-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-pinterest" aria-hidden="true" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
+                  <img className="img-fluid" src={Bagya} alt="" />
                 </div>
                 <div className="info-text">
                   <h3>
-                    <a href="#">Patric Green</a>
+                    <h6>Bhagya Indimagedara</h6>
                   </h3>
-                  <p>Front-end Developer</p>
+                  <p>2nd Year Undergraduate</p>
                 </div>
-              </div> */}
-            {/* <!-- Team Item Ends --> */}
-            {/* </div> */}
-
-            {/*  <div className="col-lg-3 col-md-6 col-xs-12"> */}
-            {/* <!-- Team Item Starts --> */}
-            {/*  <div className="team-item wow fadeInUp" data-wow-delay="0.6s">
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 col-xs-12">
+              <div className="team-item wow fadeInUp" data-wow-delay="0.2s">
                 <div className="team-img">
-                  <img
-                    className="img-fluid"
-                    src="assets/img/team/team-03.jpg"
-                    alt=""
-                  />
-                  <div className="team-overlay">
-                    <div className="overlay-social-icon text-center">
-                      <ul className="social-icons">
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-twitter-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-google" aria-hidden="true" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-facebook-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-pinterest" aria-hidden="true" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
+                  <img className="img-fluid" src={Chamodi} alt="" />
                 </div>
                 <div className="info-text">
                   <h3>
-                    <a href="#">Paul Kowalsy</a>
+                    <h6>Chamodi Thennakoon</h6>
                   </h3>
-                  <p>Lead Designer, TNW</p>
+                  <p>4th Year Undergraduate</p>
                 </div>
-              </div> */}
-            {/* <!-- Team Item Ends --> */}
-            {/* </div> */}
-
-            {/* <div className="col-lg-3 col-md-6 col-xs-12"> */}
-            {/* <!-- Team Item Starts --> */}
-            {/* <div className="team-item wow fadeInUp" data-wow-delay="0.8s">
-                <div className="team-img">
-                  <img
-                    className="img-fluid"
-                    src="assets/img/team/team-04.jpg"
-                    alt=""
-                  />
-                  <div className="team-overlay">
-                    <div className="overlay-social-icon text-center">
-                      <ul className="social-icons">
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-twitter-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-google" aria-hidden="true" />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i
-                              className="lni-facebook-filled"
-                              aria-hidden="true"
-                            />
-                          </a>
-                        </li>
-                        <li>
-                          <a href="#">
-                            <i className="lni-pinterest" aria-hidden="true" />
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-                <div className="info-text">
-                  <h3>
-                    <a href="#">Jhon Doe</a>
-                  </h3>
-                  <p>Back-end Developer, ASUS</p>
-                </div>
-              </div> */}
-            {/* <!-- Team Item Ends --> */}
-            {/* </div> */}
-
-
-
+              </div>
+            </div>
           </div>
-          {/* <a
-            href="#"
-            className="btn btn-common mt-30 wow fadeInUp"
-            data-wow-delay="1.9s"
-            style={{ color: "white" }}
-          >
-            Meet all speakers
-          </a> */}
         </div>
       </section>
     </>
